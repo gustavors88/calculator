6f022f4654f138486aedac50c1b8477f6c4cb5df
gustavors88 <gustavors88@gmail.com>
2026-07-01 01:42:02 +0900

    Level 1: Finished step 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,50 @@
 package org.example;
 
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        String cont = "";
+        while (!cont.equals("exit"))  {
+            // obtain the data
+            System.out.print("Enter the first number: ");
+            int operand1 = sc.nextInt();
+            System.out.print("Enter the second number: ");
+            int operand2 = sc.nextInt();
+            System.out.print("Enter an arithmetic operator: ");
+            char operator = sc.next().charAt(0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            // perform the calculation
+            int result = 0;
+            switch (operator) {
+                case '-':
+                    result = operand1 - operand2;
+                    break;
+                case '+':
+                    result = operand1 + operand2;
+                    break;
+                case '*':
+                    result = operand1 * operand2;
+                    break;
+                case '/':
+                    if (operand2 == 0) {
+                        System.out.println("Error: Division by 0 cannot be performed.");
+                        return;
+                    }
+                    else {
+                        result = operand1 / operand2;
+                    }
+                    break;
+                default:
+                    System.out.println("Error: Invalid operator.");
+                    return;
+            }
+            System.out.println("Result: " + result);
+            System.out.println("Would you like to calculate more? (Enter exit to quit)");
+            cont = sc.next();
         }
+
     }
 }
