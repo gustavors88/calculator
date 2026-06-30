5785c1b1fbd00444ff0d99f2cc93f8738e4bb7d8
gustavors88 <gustavors88@gmail.com>
2026-07-01 01:50:23 +0900

    Level 1: Finished step 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final int SIZE = 10;
     public static void main(String[] args) {
+        int[] results = new int[SIZE];
+        int index = 0;
         Scanner sc = new Scanner(System.in);
         String cont = "";
         while (!cont.equals("exit"))  {
@@ -41,6 +44,14 @@ public class Main {
                     System.out.println("Error: Invalid operator.");
                     return;
             }
+            // storing results and updating index
+            results[index] = result;
+            index++;
+            // restarts index if index goes outside the array
+            if (index == SIZE) {
+                index = 0;
+            }
+            // showing results
             System.out.println("Result: " + result);
             System.out.println("Would you like to calculate more? (Enter exit to quit)");
             cont = sc.next();
